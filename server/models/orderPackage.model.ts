bfaac39d0a5b76fbe56b19a7df88546dbd776c15
tsiporaShub <t8506671@gmail.com>
2024-05-23 19:29:20 +0300

    create order package model

---
+++ server/models/orderPackage.model.ts
@@ -0,0 +1,16 @@
+import mongoose from '../services/DBconnect.service';
+import { Schema } from "mongoose";
+
+import date from 'date-and-time';
+const datePattern = date.compile('YYYY/MM/DD')
+const hourPattern = date.compile('hh:mm')
+
+const orderPackage_schema = new Schema({
+    id: { type: Number },
+    packageId: { type: Number },
+    date: { type: datePattern },
+    beginingHour: { type: hourPattern },
+    endHour: { type: hourPattern },
+});
+
+export default mongoose.model("orderPackage_model", orderPackage_schema);
