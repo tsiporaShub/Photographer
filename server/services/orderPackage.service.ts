8e49c9cd649e5d86ca4efd2434188e295e1ceb70
tsiporaShub <t8506671@gmail.com>
2024-05-28 21:31:54 +0300

    create updateOrderPackage and deleteOrderPackage fuunctions to orderPackage service

--- server/services/orderPackage.service.ts
+++ server/services/orderPackage.service.ts
@@ -33,47 +33,85 @@ export const addOrderPackage = async function (req: Request, res: Response) {
         await orderPackage_model.insertMany(newOrderPackage);
         res.send('Add new  order package succeeded');
     } catch (err) {
-        res.status(409).send(''+err)
+        res.status(409).send('' + err)
     }
 }
 
 
 export const updateOrderPackage = async function (req: Request, res: Response) {
-
+    try {
+        const data = req.body;
+        const newOrderPackage = {
+            id: data.id,
+            packageId: data.packageId,
+            date: data.date,
+            beginingHour: data.beginingHour,
+            endHour: data.endHour,
+        }
+        await isCorrectorderPackage(newOrderPackage);
+        const id = req.params.id;
+        if (await photographyPackage_model.findOne({ id }) === null) {
+            res.status(404).send('order package not found')
+            return;
+        }
+        await orderPackage_model.updateOne({
+            id
+        }, {
+            $set: {
+                packageId: data.packageId,
+                date: data.date,
+                beginingHour: data.beginingHour,
+                endHour: data.endHour,
+            }
+        })
+        res.send('Update ' + id + ' succeeded');
+    } catch (err) {
+        res.status(409).send('' + err)
+    }
 }
 
 
 export const deleteOrderPackage = async function (req: Request, res: Response) {
-
+    try {
+        const id = req.params.id;
+        if (await orderPackage_model.findOne({ id }) === null) {
+            res.status(404).send('order package not found')
+            return;
+        }
+        await orderPackage_model.deleteOne({ id });
+        res.send('Delete ' + id + ' succeeded');
+    } catch (err) {
+        res.status(409).send('error...');
+    }
 }
 
-const isCorrectorderPackage =async function (orderPackage:any) {
-    if( isNaN(orderPackage.packageId) ){
+const isCorrectorderPackage = async function (orderPackage: any) {
+    if (isNaN(orderPackage.packageId)) {
         throw new Error("invalid packageId");
     }
-    if(!await photographyPackage_model.findOne({"id":orderPackage.packageId})){
+    if (!await photographyPackage_model.findOne({ "id": orderPackage.packageId })) {
         throw new Error("packageId not found");
     }
-    if(!date.isValid(orderPackage.date, datePattern)){
+    if (!date.isValid(orderPackage.date, datePattern)) {
         throw new Error('invalid date');
     }
-    if(!date.isValid(orderPackage.beginingHour, hourPattern)){
-        throw new Error("invalid beginingHour");  
+    if (!date.isValid(orderPackage.beginingHour, hourPattern)) {
+        throw new Error("invalid beginingHour");
     }
-    if(!date.isValid(orderPackage.endHour, hourPattern)){
+    if (!date.isValid(orderPackage.endHour, hourPattern)) {
         throw new Error("invalid endHour");
     }
-    if(!(orderPackage.endHour>orderPackage.beginingHour)){
+    if (!(orderPackage.endHour > orderPackage.beginingHour)) {
         throw new Error("beginingHour big from endHour");
     }
-    if(orderPackage.date<date.format(new Date(),datePattern)){
+    if (orderPackage.date < date.format(new Date(), datePattern)) {
         throw new Error("past date");
     }
-    const allOrdersInThisDate=await orderPackage_model.find({"date":orderPackage.date});
-    allOrdersInThisDate.sort((a:any,b:any)=>{return a.beginingHour<b.beginingHour?-1:1});
-    allOrdersInThisDate.forEach((order:any) => {
-        if(order['endHour']>orderPackage.beginingHour){
-            if(order['beginingHour']<orderPackage.endHour){
+    const allOrdersInThisDate = await orderPackage_model.find({ "date": orderPackage.date });
+    allOrdersInThisDate.sort((a: any, b: any) => { return a.beginingHour < b.beginingHour ? -1 : 1 });
+    allOrdersInThisDate.forEach((order: any) => {
+        if (order['endHour'] > orderPackage.beginingHour) {
+            if (order['beginingHour'] < orderPackage.endHour) {
                 throw new Error("the time is not available");
             }
         }
