2af3aef641cb34eb23fe3c73794db69426fa193e
tsiporaShub <t8506671@gmail.com>
2024-06-05 11:16:37 +0300

    create business controller

---
+++ server/controllers/business.controller.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+
+import express from 'express';
+const app = express();
+
+import bodyParser from 'body-parser';
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+import { getBusinessDetails, updateBusinessDetails } from '../services/business.service';
+
+
+export const get = async (req: Request, res: Response) => {    
+    await getBusinessDetails(req, res);
+};
+
+export const put = async (req: Request, res: Response) => {
+    await updateBusinessDetails(req, res);
+}
+
+export default app;
