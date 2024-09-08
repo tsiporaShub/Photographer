02ce0068189fe11d5fb212ea31a23ff9a61ba3ac
tsiporaShub <t8506671@gmail.com>
2024-09-08 14:51:32 +0300

    create router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,21 @@
-import './App.css'
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import TopNavComponent from './components/topNav.component';
+import SigninFormComponent from './components/signin.component.tsx';
+import SignupFormComponent from './components/signup.component.tsx';
+import OrderFormComponent from './components/orderPackage.tsx';
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <Router>
+       <Routes>
+         <Route path="/" element={<TopNavComponent />} />
+         <Route path="/signin" element={<SigninFormComponent />} />
+         <Route path="/signup" element={<SignupFormComponent />} />
+         <Route path="/order" element={<OrderFormComponent />}>
+           {/* <Route path=":id" element={<OrderFormComponent />} /> */}
+         </Route>
+       </Routes>
+    </Router>
+  );
 }
-
-export default App
+export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,23 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.tsx'
-// import './index.css'
+import App from './App.tsx'
+import './index.css'
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme.ts';
-import TopNavComponent from './components/topNav.component'
-import SigninFormComponent from './components/signin.component.tsx'
-import SignupFormComponent from './components/signup.component.tsx'
-import OrderFormComponent from './components/orderPackage.tsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      {/* <App /> */}
-      <TopNavComponent></TopNavComponent>
-      <SigninFormComponent></SigninFormComponent>
-      <SignupFormComponent></SignupFormComponent>
-      <OrderFormComponent></OrderFormComponent>
+      <App />
     </ThemeProvider>
   </React.StrictMode>,
 )
