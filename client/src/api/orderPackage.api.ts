5209a8a4e17f78f7d7a4f134dc2901f11976d638
tsiporaShub <t8506671@gmail.com>
2024-09-23 12:12:56 +0300

    sending a token in addOrderPackage api

--- client/src/api/orderPackage.api.ts
+++ client/src/api/orderPackage.api.ts
@@ -3,15 +3,18 @@ import { domain } from '../config';
 import { OrderPackage } from '../interfaces/orderPackage.interface';
 
 export const addOrderPackage = async (data: OrderPackage) => {
+    const token = sessionStorage.getItem('token');
+
     try {
         const response = await axios.post(`${domain}/OrderPackage`, data, {
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'token': token
             }
-        })
+        });
         return response.data;
     } catch (error) {
-        console.error('error in api request of add order package', error);
+        console.error('Error in API request of adding an order package', error);
         throw error;
     }
 }
